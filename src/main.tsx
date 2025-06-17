4cc1b2e52e3a30f1647000ee79e363d68a7575d0
Maggie Ellinwood <mellinwood1023@gmail.com>
2025-06-17 13:53:48 -0400

    styles global

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';  // This is your top-level component      // Optional global styles
-import './styles/global.css'; // Assuming you have a global CSS file
+import '../styles/global.css'; // Assuming you have a global CSS file
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
